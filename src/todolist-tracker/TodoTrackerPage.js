cacaac17e34779663a3432960127c10f84b431e2
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-05-17 20:34:49 -0400

    error

--- src/todolist-tracker/TodoTrackerPage.js
+++ src/todolist-tracker/TodoTrackerPage.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import { addTodo } from '../utils/todo-api.js';
+import { addTask, getTodos } from '../utils/todo-api.js';
 import './TodoTrackerPage.css';
 
 export default class TodoTrackerPage extends Component {
@@ -8,29 +8,63 @@ export default class TodoTrackerPage extends Component {
     todos: []
   }
 
+  async componentDidMount() {
+    try {
+      const todos = await getTodos();
+      this.setState({ todos: todos });
+
+      console.log(todos);
+    }
+    catch (err) {
+      console.log(err);
+    }
+  }
+
   handleAdd = async e => {
     e.preventDefault();
-    const { newTask } = this.state;
-    const addedTask = await addTodo({ task: newTask });
-
-    console.log('ADDED', addedTask);
+    const { newTask, todos } = this.state;
+    
+    try {
+      const addedTask = await addTask({ task: newTask });
+      const updatedTodos = [...todos, addedTask];
+      this.setState({
+        todos: updatedTodos,
+        newTask: ''
+      });
+    }
+    catch (err){
+      console.log(err.message);
+    }
   }
 
   handleNewTask = ({ target }) => {
     this.setState({ newTask: target.value });
   }
 
-  render() {
-    const { newTask } = this.state;
+handleDelete = async id => {
+  console.log('you want to delete', id);
+}
 
-    return (
-      <div className="TodoTrackerPage">
-        <form onSubmit={this.handleAdd}>
-          Add New Task:
-          <input value={newTask} onChange={this.handleNewTask}/>
-        </form>
+render() {
+  const { newTask, todos } = this.state;
 
-      </div>
-    );
-  }
+  return (
+    <div className="TodoTrackerPage">
+      <form onSubmit={this.handleAdd}>
+        Add New Task:
+        <input value={newTask} onChange={this.handleNewTask}/>
+      </form>
+
+      <ul>
+        {todos.map(task => (
+          <li key={task.id}>
+            <h2>{task.task}</h2>
+            <button>X</button>
+          </li>
+        ))}
+      </ul>
+
+    </div>
+  );
+}
 }
